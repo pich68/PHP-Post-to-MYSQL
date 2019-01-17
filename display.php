e0b4b1181cc4c47155f114f3e02a8475ef8d1593
pich68 <pich68@me.com>
2019-01-17 20:14:56 +0100

    Add files via upload
    generate pdf button added

--- display.php
+++ display.php
@@ -5,8 +5,18 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <link rel="stylesheet" href="style.css">
-        <title>HTML Form Tutorial - Display</title>
+        <title>Display db rows</title>
     </head>
+	<form method="post" action="">
+		<div class="col-sm-6 form-group">
+			<button type="submit" name="generatepdf" class="btn btn-lg btn-success btn-block">Generate PDF</button>
+		</div>
+	<form method="post" action="">
+		<div class="col-sm-6 form-group">
+			<button type="submit" name="sendmail" class="btn btn-lg btn-success btn-block">Mail Me</button>
+		</div>	
+	</form>
+		
     <body style="max-width: 900px;">
         <?php
         // Connect to database
@@ -25,9 +35,7 @@
                 <tr>
                     <th>Name</th>
                     <th>Email</th>
-                    <th>Frequency</th>
-                    <th>Interests</th>
-                    <th>Country</th>
+                    <th>Catalog</th>
                     <th>Message</th>
                 </tr>
             </thead>
@@ -36,10 +44,8 @@
                     <tr>
                         <td><?php _e($result->name); ?></td>
                         <td><?php _e($result->email); ?></td>
-                        <td><?php _e($result->frequency); ?></td>
-                        <td><?php _e(implode(', ', unserialize($result->interests))); ?></td>
-                        <td><?php _e($result->country); ?></td>
-                        <td><?php _e($result->comment); ?></td>
+                        <td><?php _e($result->catalog); ?></td>
+                        <td><?php _e($result->description); ?></td>
                     </tr>
                 <?php endforeach; ?>
             </tbody>
@@ -47,9 +53,7 @@
                 <tr>
                     <th>Name</th>
                     <th>Email</th>
-                    <th>Frequency</th>
-                    <th>Interests</th>
-                    <th>Country</th>
+                    <th>Catalog</th>
                     <th>Message</th>
                 </tr>
             </tfoot>
