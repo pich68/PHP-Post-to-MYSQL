ffd821e020d4ef5868431b4c2eeb12b25e201a15
pich68 <pich68@me.com>
2019-01-18 16:42:33 +0100

    Add files via upload
    tcpdf library used to generate pdf

--- display.php
+++ display.php
@@ -1,62 +1,84 @@
-<?php require_once('load.php'); ?>
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <link rel="stylesheet" href="style.css">
-        <title>Display db rows</title>
-    </head>
-	<form method="post" action="">
-		<div class="col-sm-6 form-group">
-			<button type="submit" name="generatepdf" class="btn btn-lg btn-success btn-block">Generate PDF</button>
-		</div>
-	<form method="post" action="">
-		<div class="col-sm-6 form-group">
-			<button type="submit" name="sendmail" class="btn btn-lg btn-success btn-block">Mail Me</button>
-		</div>	
-	</form>
-		
-    <body style="max-width: 900px;">
-        <?php
-        // Connect to database
-        $mysql = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-        
-        // Select data
-        $resource = $mysql->query("SELECT * FROM users");
-        
-        // Loop and fetch
-        while($row = $resource->fetch_object()) {
-        	$results[] = $row;
-        }
-        ?>
-        <table class="table users">
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Catalog</th>
-                    <th>Message</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php foreach ( $results as $result ) : ?>
-                    <tr>
-                        <td><?php _e($result->name); ?></td>
-                        <td><?php _e($result->email); ?></td>
-                        <td><?php _e($result->catalog); ?></td>
-                        <td><?php _e($result->description); ?></td>
-                    </tr>
-                <?php endforeach; ?>
-            </tbody>
-            <tfoot>
-                <tr>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Catalog</th>
-                    <th>Message</th>
-                </tr>
-            </tfoot>
-        </table>
-    </body>
-</html>
+ <?php  
+ function fetch_data()  
+ {  
+      $output = '';  
+      $connect = mysqli_connect("localhost", "root", "", "forms");  
+      $sql = "SELECT * FROM users";  
+      $result = mysqli_query($connect, $sql);  
+      while($row = mysqli_fetch_array($result))  
+      {       
+      $output .= '<tr>  
+                          <td>'.$row["name"].'</td>  
+                          <td>'.$row["email"].'</td>  
+                          <td>'.$row["catalog"].'</td>  
+                          <td>'.$row["description"].'</td>  
+                     </tr>  
+                          ';  
+      }  
+      return $output;  
+ }  
+ if(isset($_POST["create_pdf"]))  
+ {  
+      require_once('tcpdf.php');  
+      $obj_pdf = new TCPDF('P', PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);  
+      $obj_pdf->SetCreator(PDF_CREATOR);  
+      $obj_pdf->SetTitle("Export HTML Table data to PDF using TCPDF in PHP");  
+      $obj_pdf->SetHeaderData('', '', PDF_HEADER_TITLE, PDF_HEADER_STRING);  
+      $obj_pdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));  
+      $obj_pdf->setFooterFont(Array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));  
+      $obj_pdf->SetDefaultMonospacedFont('helvetica');  
+      $obj_pdf->SetFooterMargin(PDF_MARGIN_FOOTER);  
+      $obj_pdf->SetMargins(PDF_MARGIN_LEFT, '5', PDF_MARGIN_RIGHT);  
+      $obj_pdf->setPrintHeader(false);  
+      $obj_pdf->setPrintFooter(false);  
+      $obj_pdf->SetAutoPageBreak(TRUE, 10);  
+      $obj_pdf->SetFont('helvetica', '', 12);  
+      $obj_pdf->AddPage();  
+      $content = '';  
+      $content .= '  
+      <h3 align="center">Export HTML Table data to PDF using TCPDF in PHP</h3><br /><br />  
+      <table border="1" cellspacing="0" cellpadding="5">  
+           <tr>  
+                <th width="30%">Name</th>  
+                <th width="10%">Email</th>  
+                <th width="45%">Catalog</th>  
+                <th width="10%">Description</th>  
+           </tr>  
+      ';  
+      $content .= fetch_data();  
+      $content .= '</table>';  
+	  ob_end_clean();
+      $obj_pdf->writeHTML($content);  
+      $obj_pdf->Output('sample.pdf', 'I');  
+ }  
+ ?>  
+ <!DOCTYPE html>  
+ <html>  
+      <head>  
+           <title>Webslesson Tutorial | Export HTML Table data to PDF using TCPDF in PHP</title>  
+           <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />            
+      </head>  
+      <body>  
+           <br /><br />  
+           <div class="container" style="width:700px;">  
+                <h3 align="center">Export HTML Table data to PDF using TCPDF in PHP</h3><br />  
+                <div class="table-responsive">  
+                     <table class="table table-bordered">  
+                          <tr>  
+                               <th width="30%">Name</th>  
+                               <th width="10%">Email</th>  
+                               <th width="45%">Catalog</th>  
+                               <th width="10%">Description</th>  
+                          </tr>  
+                     <?php  
+                     echo fetch_data();  
+                     ?>  
+                     </table>  
+                     <br />  
+                     <form method="post">  
+                          <input type="submit" name="create_pdf" class="btn btn-danger" value="Create PDF" />  
+                     </form>  
+                </div>  
+           </div>  
+      </body>  
+ </html>  
